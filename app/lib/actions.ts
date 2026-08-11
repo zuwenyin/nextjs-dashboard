a9679d62c9ea5f9b0dba1b9d3a82cf9696db07d5
Rise <wenyinzu@163.com>
2026-08-11 14:03:54 +0800

    feat(auth): 添加用户认证功能
    实现基于 NextAuth 的登录/登出逻辑，包括凭据验证、错误处理及登录表单状态管理

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,7 +3,8 @@ import { z } from "zod";
 import postgres from "postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
-import { ca } from "zod/v4/locales";
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
@@ -118,4 +119,23 @@ export async function deleteInvoice(id: string) {
   `;
 
   revalidatePath("/dashboard/invoices");
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
---
+++ auth.config.ts
@@ -0,0 +1,28 @@
+import type { NextAuthConfig } from "next-auth";
+
+export const authConfig = {
+  pages: {
+    signIn: "/login",
+  },
+  session: {
+    strategy: "jwt",
+    maxAge: 2 * 60 * 60, // 2小时
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      console.log(auth, "auth:::");
+      console.log(nextUrl, "nextUrl:::");
+
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL("/dashboard", nextUrl));
+      }
+      return true;
+    },
+  },
+  providers: [],
+} satisfies NextAuthConfig;
